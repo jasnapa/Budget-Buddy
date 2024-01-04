3ab6ea5bbab5577ae778d8930748fbf35f073d83
jasnapa <jasnajass661@gmail.com>
2024-01-04 11:46:46 +0530

    expense added

---
+++ Server/controller/expenseController.js
@@ -0,0 +1,12 @@
+
+
+
+export async function addExpense(req, res) {
+    try {
+       const {amount, category, date } = req.body
+       
+
+    } catch (error) {
+        console.error(error);
+    }
+}
