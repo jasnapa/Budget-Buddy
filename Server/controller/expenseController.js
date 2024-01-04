f72fb228be078ff11cffbbf411fdb1fefaff1c0b
jasnapa <jasnajass661@gmail.com>
2024-01-04 19:26:10 +0530

    budget controller added

--- Server/controller/expenseController.js
+++ Server/controller/expenseController.js
@@ -1,36 +1,55 @@
 import expenseModel from "../model/expenseModel.js";
-
+import UserModel from "../model/userModel.js";
 
 export async function addExpense(req, res) {
-    try {
-        const { amount, category, date } = req.body
+  try {
+    const { amount, category, date } = req.body;
 
-        const _id = req.userId
+    const _id = req.userId;
 
-        const user = await expenseModel.create({ amount, category, date, user: _id })
+    const user = await expenseModel.create({
+      amount,
+      category,
+      date,
+      user: _id,
+    });
 
-        return res.status(201).json({ status: true, message: "Expense added Successfully" })
-
-    } catch (error) {
-        console.error(error);
-    }
+    return res
+      .status(201)
+      .json({ status: true, message: "Expense added Successfully" });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getExpenses(req, res) {
-    try {
-        const expenses = await expenseModel.find({ user: req.userId })
-        return res.status(200).json({ status: true, expenses })
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const expenses = await expenseModel.find({ user: req.userId }).populate('user')
+    return res.status(200).json({ status: true, expenses });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function addBudget(req, res) {
+  try {
+    const _id = req.userId;
+    const { budget } = req.body;
+    const result = await UserModel.findByIdAndUpdate(_id, {
+      $set: { monthlyBudget: budget },
+    });
+    return res.status(200).json({ status: true, message:'Budget added' ,result});
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function deleteExpense(req, res) {
-    try {
-        const { id } = req.params;
-        await expenseModel.findByIdAndDelete(id)
-        res.status(200).json({status: true,message: 'Expense Deleted'})
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const { id } = req.params;
+    await expenseModel.findByIdAndDelete(id);
+    res.status(200).json({ status: true, message: "Expense Deleted" });
+  } catch (error) {
+    console.error(error);
+  }
 }
--- Server/model/expenseModel.js
+++ Server/model/expenseModel.js
@@ -4,7 +4,7 @@ const expenseSchema = new mongoose.Schema({
 
     user :  {
         type : mongoose.Schema.Types.ObjectId ,
-        ref : 'Users' ,
+        ref : 'users' ,
         required : true
       },
     amount: {
